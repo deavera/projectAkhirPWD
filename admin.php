32377d136843eb5b86900c2f08e00aa3e0d6d695
mahdiyaarkaris <mahdiyaarkaris@gmail.com>
2026-05-06 15:42:52 +0700

    nav up

--- admin.php
+++ admin.php
@@ -41,10 +41,6 @@ include 'koneksi.php';
             <a class="nav-link active" href="admin.php">Admin</a>
           </li>
         </ul>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
       </div>
     </div>
   </nav>
